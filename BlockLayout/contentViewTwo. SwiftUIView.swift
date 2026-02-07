1fc5fedd10380b566457cd8918930981be92a6cd
Machid Nasiri <mnasiri@lcs.on.ca>
2026-02-07 11:01:36 -0500

    completed my second layout

---
+++ BlockLayout/contentViewTwo. SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  contentViewTwo. SwiftUIView.swift
+//  BlockLayout
+//
+//  Created by Machid on 2/7/26.
+//
+
+import SwiftUI
+
+struct contentViewTwo__SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    contentViewTwo__SwiftUIView()
+}
