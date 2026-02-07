83251f4f0551a95be86d281e18861d6abef26175
Machid Nasiri <mnasiri@lcs.on.ca>
2026-02-07 10:09:47 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Machid on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
